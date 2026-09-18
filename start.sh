aa201a311b8cdaf0a77bdf35269f7ae1a96a483e
AanchalRathi <aanchalrathiwork@gmail.com>
2026-09-18 16:03:53 +0530

    reduce celery concurrency, was spawning 8 workers and running out of memory on render free tier

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-celery -A celery_app worker --loglevel=info &
+celery -A celery_app worker --loglevel=info --concurrency=2 --without-mingle --without-gossip --without-heartbeat &
 uvicorn api:app --host 0.0.0.0 --port 8000
